3e033bf547c1e8d01b6990bf1331977070fe9903
lashum <lashum@rpi.edu>
2023-09-19 16:30:56 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import code
+import numpy as np
+from scipy.io import wavfile
+from scipy import signal
+
+np.set_printoptions(precision = 3)
+
+sample_rate, dt = wavfile.read('drums.wav')
+dt = dt[0:,0]/2 + dt[0:,1]/2
+
+def getPeaks(threshold = 0, distance = 0):
+    return signal.find_peaks(dt, threshold = threshold, distance = distance)[0]
+
+bpm = 128
+def s2b(samples):
+    return samples / sample_rate * bpm / 60
+
+def getNotes(threshold = 0, distance = 0):
+    return s2b(getPeaks(threshold, distance)) * (2 ** 2)
+
+fp = getPeaks(1000, 500)
+fb = s2b(fp)
+fn = fb * (2 ** 2)
+
+code.interact(local = globals())
