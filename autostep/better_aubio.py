8102943acd9019de2c8e419100cca3996521f2ed
trustytrojan <tarbot24@hotmail.com>
2023-10-13 16:20:41 -0400

    better_aubio.onset supports different methods

--- autostep/better_aubio.py
+++ autostep/better_aubio.py
@@ -1,7 +1,10 @@
 from subprocess import check_output
+from typing import Literal
 
 def tempo(song_filename: str) -> int:
 	return int(float(check_output(f"aubio tempo {song_filename}", shell=True, text=True).replace(" bpm", "")))
 
-def onset(song_filename: str) -> tuple[float, ...]:
-	return tuple(float(timestamp) for timestamp in check_output(f"aubio onset {song_filename}", shell=True, text=True).splitlines())
+OnsetMethod = Literal['default', 'energy', 'hfc', 'complex', 'phase', 'specdiff', 'kl', 'mkl', 'specflux']
+
+def onset(song_filename: str, method: OnsetMethod) -> tuple[float, ...]:
+	return tuple(float(timestamp) for timestamp in check_output(f"aubio onset -m {method} {song_filename}", shell=True, text=True).splitlines())
