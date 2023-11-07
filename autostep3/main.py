c15903501108ee26cd349889812ae4271b6a45e1
lashum <lashum@rpi.edu>
2023-11-07 16:59:54 -0500

    Fix ssc functions

--- autostep3/main.py
+++ autostep3/main.py
@@ -2,22 +2,26 @@ from util import make_chart_code, sample2note, separate_drums
 from sys import argv, stderr
 import soundfile as sf
 import better_aubio
+from ssc import *
 from autochart.inout import convertToSSC, injectSSCToChart
 
 def main():
-	if len(argv) != 2:
-		print(f"Usage: {argv[0]} <audio_file> <difficulties>", file=stderr)
+	if len(argv) != 4:
+		print(f"Usage: {argv[0]} <audio_file> <bpm> <offset> <output_file>",
+		 file=stderr)
 		exit(1)
 
 	drums_path, audio_file_name = separate_drums(argv[1])
 	samplerate = sf.info(drums_path).samplerate
 
-	tempo = better_aubio.tempo_cli(drums_path)
+	bpm, offset, output_file = argv[2:]
 	onsets = better_aubio.onset(drums_path, 'energy')
-	onsets_sixteenth_notes = sample2note(onsets, samplerate, tempo, 16)
+	onsets_sixteenth_notes = sample2note(onsets, samplerate, bpm, 16)
 	make_chart_code(onsets_sixteenth_notes, 16, "code.txt")
 	convertToSSC("code.txt", "chart.txt")
 	injectSSCToChart("chart.txt", "generated/" + audio_file_name + ".ssc", 2)
+
+	create_file('audio_file_name', bpm, offset, 1, 0, 'chart.txt', output_file)
 
 # Call main() ONLY when this module is EXECUTED and not IMPORTED.
 if __name__ == "__main__":
--- autostep3/ssc.py
+++ autostep3/ssc.py
@@ -17,97 +17,14 @@ DEFAULT_ATTRS = {
 
 DIFFICULTY_NAMES = ["Beginner", "Easy", "Medium", "Hard", "Challenge"]
 
-class SscChart:
-	def __init__(self,
-		difficulty: str,
-		meter: int,
-		stepstype: str = "dance-single",
-		description: Optional[str] = None,
-		notes: list[str] = []
-	):
-		self.difficulty = difficulty
-		self.meter = meter
-		self.stepstype = stepstype
-		self.description = description
-		self.notes = notes
-
-class SscFile:
-	@staticmethod
-	def open(filename: str) -> 'SscFile':
-		with open(filename, "r") as file:
-			file_text = file.read()
-
-		def find_value(key: str):
-			key = f"#{key}:"
-			index = file_text.index(key) + len(key)
-			return file_text[index : file_text.index(";", index)]
-
-		return SscFile(
-			title=find_value("TITLE"),
-			music_filename=find_value("MUSIC"),
-			offset=find_value("OFFSET"),
-			artist=find_value("ARTIST"),
-			bg_filename=find_value("BACKGROUND")
-		)
-
-		# charts = []
-		# while True:
-		# 	match file_text.find("//-----"):
-		# 		case -1:
-		# 			break
-		# 		case chart_idx:
-
-	def __init__(self,
-		title: str,
-		music_filename: str,
-		bpm: int,
-		offset: float,
-		artist: Optional[str] = None,
-		bg_filename: Optional[str] = None,
-		charts: list[SscChart] = []
-	):
-		self.title = title
-		self.music_filename = music_filename
-		self.bpm = bpm
-		self.offset = offset
-		self.artist = artist
-		self.bg_filename = bg_filename
-		self.charts = charts
-
-	def save_as(self, filename: str):
-		with open(filename, "w") as file:
-			write_attr = lambda key, value: file.write(f"#{key}:{value};\n")
-			write_attr("VERSION", "0.83")
-			write_attr("TITLE", self.title)
-			write_attr("ARTIST", self.artist)
-			write_attr("MUSIC", self.music_filename)
-			if self.bg_filename is not None:
-				write_attr("BACKGROUND", self.bg_filename)
-			write_attr("OFFSET", str(self.offset))
-			write_attr("BPMS", "0.000={3f}")
-			for chart in self.charts:
-				file.write(f"//--------------- {chart.stepstype} -----------------")
-				#write_attr("NOTEDATA", "")
-				write_attr("STEPSTYPE", chart.stepstype)
-				write_attr("DIFFICULTY", chart.difficulty)
-				write_attr("METER", chart.meter)
-				write_attr("NOTES", "\n")
-				for line in chart.notes:
-					file.write(line + "\n")
-				file.write(";\n")
-
 def create_file(
-	ssc_file: str,
-	*,
-	music_filename: str,
-	title: str,
-	artist: str,
-	bpm: int,
-	offset: float,
-	rating: int, # diff rating
-	diff_position: int, # position in the ITG diff selector
+	audio_file: str,
+	bpm: float,
+	offset: str,
+	rating: int,
+	difficulty_pos: int,
 	chart_file: str,
-	bg_filename: Optional[str] = None,
+	output_file: str
 ) -> None:
 	attributes = DEFAULT_ATTRS.copy()
 
@@ -116,13 +33,16 @@ def create_file(
 	attributes.update(new_attributes)
 
 	attribute_text = attributeText(attributes)
-	chart_text = chartText(chart_file)
+	chart_text = chartText(chart_file, rating, difficulty_pos)
 
-	text = attribute_text + chart_text
+	text = attribute_text + '\n' + chart_text
+
+	with open(output_file, 'w') as f:
+		f.write(text)
 
 def attributeText(attributes):
 	def attributeToLine(item):
-		return f'#{item[0].upper}:{item[1]};'
+		return f'#{item[0].upper()}:{item[1]};'
 
 	lines = [attributeToLine(item) for item in attributes.items()]
 
@@ -132,15 +52,15 @@ def chartText(chart_filepath: str, rating: int, diff_position: int):
 	difficulty = DIFFICULTY_NAMES[diff_position]
 
 	return f"""//--------------- dance-single -  ----------------
-	#NOTEDATA:;
-	#STEPSTYPE:dance-single;
-	#DESCRIPTION:;
-	#DIFFICULTY:{difficulty};
-	#METER:{rating};
-	#RADARVALUES:0,0,0,0,0;
-	#NOTES:
-	{loadTxt(chart_filepath)}
-	;"""
+#NOTEDATA:;
+#STEPSTYPE:dance-single;
+#DESCRIPTION:;
+#DIFFICULTY:{difficulty};
+#METER:{rating};
+#RADARVALUES:0,0,0,0,0;
+#NOTES:
+{loadTxt(chart_filepath)}
+;"""
 
 def loadTxt(chart_filepath: str):
 	with open(chart_filepath, 'r') as f:
