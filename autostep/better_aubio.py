3de2091338a9dfec389cfbf8d2f84181dff66a25
trustytrojan <tarbot24@hotmail.com>
2023-11-17 17:00:13 -0500

    get rid of old autostep, fix merge

--- autostep/better_aubio.py
+++ autostep/better_aubio.py
@@ -1,40 +1,38 @@
-from subprocess import check_output
-from typing import Literal, Optional
-from math import inf
+from typing import Callable, Literal
+try:
+	from util import NDIntArray
+except ModuleNotFoundError:
+	from autostep3.util import NDIntArray
 import numpy as np
 import aubio
+import subprocess
+import math
 
-def shell_exec(command: str) -> str:
-	return check_output(command, shell=True, text=True)
+shell_exec: Callable[[str], str]
+shell_exec = lambda command: subprocess.check_output(command, shell=True, text=True)
 
-def tempo(song_filename: str) -> int:
-	return int(float(shell_exec(f"aubio tempo {song_filename}").replace(" bpm", "")))
+tempo_cli: Callable[[str], float]
+tempo_cli = lambda filename: int(float(shell_exec(f"aubio tempo {filename}").replace(" bpm", "")))
 
-OnsetMethod = Literal['default', 'energy', 'hfc', 'complex', 'phase', 'specdiff', 'kl', 'mkl', 'specflux']
+ONSET_METHODS = "default", "energy", "hfc", "complex", "phase", "specdiff", "kl", "mkl", "specflux"
+OnsetMethod = Literal["default", "energy", "hfc", "complex", "phase", "specdiff", "kl", "mkl", "specflux"]
 
-def onset_seconds(song_filename: str, method: OnsetMethod) -> tuple[float, ...]:
-	return tuple(float(timestamp) for timestamp in shell_exec(f"aubio onset -m {method} {song_filename}").splitlines())
-
-def onset_samples(filename: str, method: OnsetMethod) -> list[int]:
-	s = aubio.source(filename)
-	o = aubio.onset(method)
-	onsets = []
-	read = inf
+# https://github.com/aubio/aubio/blob/master/python/demos/demo_onset.py
+def onset(
+	filename: str,
+	method: OnsetMethod,
+	*,
+	fft_size: int = 1024,
+	hop_size: int = 512
+) -> NDIntArray:
+	if method not in ONSET_METHODS:
+		raise ValueError("`method` is not a valid onset method")
+	s = aubio.source(filename) # type: ignore
+	o = aubio.onset(method, fft_size, hop_size) # type: ignore
+	onsets: list[int] = []
+	read = math.inf
 	while read > 0:
 		samples, read = s()
 		if o(samples):
 			onsets.append(o.get_last())
-	return onsets
-
-def quiet(song_filename: str, silence_threshold: Optional[int] = None) -> tuple[float, ...]:
-	"""
-	NOTE: In the returned tuple, even indices are `NOISY`, and odd indices are `QUIET`
-	"""
-	if silence_threshold is None:
-		output = shell_exec(f"aubio quiet {song_filename}")
-	else:
-		output = shell_exec(f"aubio quiet {song_filename} -s {silence_threshold}")
-	timestamps = []
-	for line in output.splitlines():
-		timestamps.append(float(line.replace("NOISY: ", "").replace("QUIET: ", "")))
-	return tuple(timestamps)
+	return np.array(onsets)
--- autostep/main.py
+++ autostep/main.py
@@ -1,12 +1,83 @@
-from code import interact
-from song import Song
-import numpy as np
-from detect import *
-from array2chart import *
-import test
+from typing import Any
+import soundfile as sf
+import better_aubio
+import ssc
+import autochart.inout
+import util
+import argparse
 
-np.set_printoptions(precision=3)
+CODE_FILE = "/tmp/code.txt"
+CHART_FILE = "/tmp/chart.txt"
 
-song = Song('test_audio/drums.wav', bpm = 128, offset = -0.095)
+arg_parser = argparse.ArgumentParser(description="Generate an ITGMania chart (.ssc) from an audio file containing music.")
 
-arrayToChart(aubioPeaks(song, 1024, 512, 1), song)
+# the inner dicts conform to ArgumentParser.add_argument's keyword args
+ARGUMENTS: dict[str, dict[str, Any]] = {
+	"audio_file": {
+		"help": "Path to an audio file, typically containing music"
+	},
+	"output_file": {
+		"help": "The file to save the ITGMania chart to, typically ending in .ssc"
+	},
+	"--bpm": {
+		"type": float,
+		"help": "Music tempo",
+		"required": False
+	},
+	"--offset": {
+		"type": float,
+		"help": "Music start time relative to audio start time",
+		"default": 0
+	},
+	"--division": {
+		"type": int,
+		"help": "Total number of notes in a measure",
+		"default": 16
+	},
+	"--onset_method": {
+		"choices": better_aubio.ONSET_METHODS,
+		# GOOD onset methods: default, complex, hfc
+		# BAD onset methods: energy, phase
+		"default": "default",
+		"help": "The onset (beat) detection method to use"
+	},
+	"--sample2note_method": {
+		"choices": ("default", "round", "offset", "round+offset"),
+		"default": "default"
+	}
+}
+
+for key, value in ARGUMENTS.items():
+	arg_parser.add_argument(key, **value)
+
+def main():
+	args = arg_parser.parse_args()
+
+	drums_path, _ = util.separate_drums(args.audio_file)
+	samplerate = sf.info(drums_path).samplerate
+
+	onsets = better_aubio.onset(drums_path, args.onset_method)
+
+	if args.bpm is None:
+		args.bpm = better_aubio.tempo_cli(args.audio_file)
+
+	# this is here for TESTING PURPOSES, at least for now
+	match args.sample2note_method:
+		case "default":
+			onsets_sixteenth_notes = util.sample2note(onsets, samplerate, args.bpm, 16)
+		case "round":
+			onsets_sixteenth_notes = util.sample2note_round(onsets, samplerate, args.bpm, 16)
+		case "offset":
+			onsets_sixteenth_notes = util.sample2note_offset(onsets, samplerate, args.bpm, args.offset, 16)
+		case "round+offset":
+			onsets_sixteenth_notes = util.sample2note_round_offset(onsets, samplerate, args.bpm, args.offset, 16)
+
+	util.make_chart_code(onsets_sixteenth_notes, 16, CODE_FILE) # type: ignore
+	autochart.inout.convertToSSC(CODE_FILE, CHART_FILE)
+
+	ssc.write(args.audio_file, args.bpm, args.offset, 1, 0, CHART_FILE, args.output_file)
+	autochart.inout.injectSSCToChart(CHART_FILE, args.output_file, 1)
+
+# Call main() ONLY when this module is EXECUTED and not IMPORTED.
+if __name__ == "__main__":
+	main()
